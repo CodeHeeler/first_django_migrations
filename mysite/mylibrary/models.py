ab0f16ecd5d6108c78d60e08716ec235fea078e5
RebelMerf <rebelmurf@gmail.com>
2017-04-12 01:45:04 -0400

    fleshed out model, migrated changes

--- mysite/mylibrary/models.py
+++ mysite/mylibrary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200, blank=True)
+    copyright = models.CharField(max_length=25, blank=True)
+    isbn = models.CharField(max_length=25, blank=True)
+    pages = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    keyword = models.CharField(max_length=50, null=True, blank=True)
+    # want to do another model for keywords, manytomany, but will come in later updates
